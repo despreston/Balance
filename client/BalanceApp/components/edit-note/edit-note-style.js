6fbad878bbb1dbb1f28290f2a8935b696abc8b84
Des Preston <despreston@gmail.com>
2017-01-16 23:30:32 -0500

    edit note modal header buttons

--- client/BalanceApp/components/edit-note/edit-note-style.js
+++ client/BalanceApp/components/edit-note/edit-note-style.js
@@ -2,6 +2,28 @@ import { StyleSheet } from 'react-native';
 
 export const Styles = StyleSheet.create  ({
   editNote: {
-    marginTop: 22
+    marginTop: 30
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10
+  },
+  actions: {
+    flexDirection: 'row',
+  },
+  headerText: {
+    color: '#525252',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  spacer: {
+    backgroundColor: '#CFD0D4',
+    width: 2,
+    height: 25,
+    marginHorizontal: 10
+  },
+  clear: {
+    color: '#C65D63'
   }
 });
--- client/BalanceApp/components/edit-note/edit-note.js
+++ client/BalanceApp/components/edit-note/edit-note.js
@@ -14,20 +14,37 @@ export default class EditNote extends Component {
 
   constructor (props) {
     super();
+    this.state = { isDirty: false }
+  }
+
+  getSaveTextColor () {
+    return this.state.isDirty ? {} : { color: '#CFD0D4' };
   }
 
   render () {
     return (
-      <View style={ Styles.editNote }>
+      <View style={Styles.editNote}>
         <Modal
           animationType={"slide"}
           transparent={false}
           visible={this.props.visible}
           >
-          <View style={ Styles.editNote }>
-            <TouchableHighlight onPress={this.props.onClose}>
-              <Text>{this.props.note.text}</Text>
-            </TouchableHighlight>
+          <View style={Styles.editNote}>
+            <View style={Styles.header}>
+              <TouchableHighlight onPress={this.props.onClose}>
+                <Text style={Styles.headerText}>Close</Text>
+              </TouchableHighlight>
+              <View style={Styles.actions}>
+                <TouchableHighlight>
+                  <Text style={[Styles.headerText, Styles.clear]}>Clear</Text>
+                </TouchableHighlight>
+                <View style={Styles.spacer} />
+                <TouchableHighlight>
+                  <Text style={[Styles.headerText, this.getSaveTextColor() ]}>Save</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+            <Text>{this.props.note.text}</Text>
           </View>
         </Modal>
       </View>
