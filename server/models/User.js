ced3b90870899749222e09dbd35eb2eda0274158
despreston <despreston@gmail.com>
2017-02-16 18:27:20 -0500

    updating user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,17 +2,25 @@
 const mongoose = require("mongoose");
 
 module.exports = mongoose.model("user", new mongoose.Schema({
+
   firstName: {
     required: true,
     type: String
   },
+
   lastName: {
     required: true,
     type: String
   },
+
   email: {
     index: true,
     required: true,
     type: String
-  }
+  },
+
+  lastUpdated: Date,
+
+  createdAt: Date
+
 }));
