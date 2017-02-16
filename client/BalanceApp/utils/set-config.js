3eb4286389d778ff94ea128832eeff03b8c21b50
despreston <despreston@gmail.com>
2017-02-16 18:42:48 -0500

    Development.js should be optional

--- client/BalanceApp/utils/set-config.js
+++ client/BalanceApp/utils/set-config.js
@@ -1,4 +1,4 @@
-import dev from '../config/development';
-import defaults from '../config/default';
+const defaults = require('../config/default');
+const dev = require('../config/development') || {};
 
 global.CONFIG = Object.assign({}, defaults, dev);
