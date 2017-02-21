8e5aafb9354ccf9bd0c4ac26e360615be55c86d4
despreston <despreston@gmail.com>
2017-02-20 19:10:05 -0500

    jwt auth object

--- server/auth.js
+++ server/auth.js
@@ -1,26 +1,9 @@
-const Auth0Strategy = require('passport-auth0');
-const passport = require('passport');
+let jwt = require('restify-jwt');
+let config = require('./config.json');
 
-const strategy = new Auth0Strategy({
-  domain: process.env.AUTH0_DOMAIN,
-  clientId: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:9000/callback'
-}, (accessToken, refreshToken, extraParams, profile, done) => {
-  // accessToken is the token to call Auth0 API (not needed in the most cases)
-  // extraParams.id_token has the JSON Web Token
-  // profile has all the information from the user
-  return done(null, profile);
+let jwtCheck = jwt({
+  secret: config.auth.secret,
+  audience: config.auth.id
 });
 
-passport.use(strategy);
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
-
-module.exports = passport;
+module.exports = jwtCheck;
