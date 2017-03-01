d9731ac35c91ccfe54e388e3ea27af49945eb5dd
despreston <despreston@gmail.com>
2017-03-01 16:57:27 -0500

    skip some urls in auth check --skip-ci

--- server/auth.js
+++ server/auth.js
@@ -1,9 +1,17 @@
 let jwt = require('restify-jwt');
 let config = require('./config.json');
 
+const skippedUrls = ['/', '/_health'];
+
 let jwtCheck = jwt({
   secret: config.auth.secret,
   audience: config.auth.id
 });
 
-module.exports = jwtCheck;
+module.exports = (req, res, next) => {
+  if (skippedUrls.indexOf(req.url) < 0) {
+    return jwtCheck(req, res, next);
+  }
+
+  next();
+};
