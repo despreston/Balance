bed786fa9332425bc604f7a2b5144cb761193392
despreston <despreston@gmail.com>
2017-05-02 10:15:41 -0400

    show a confirmation dialog when deleting a note. --skip-ci

--- client/BalanceApp/components/add-update/trash/trash.js
+++ client/BalanceApp/components/add-update/trash/trash.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity, Alert } from 'react-native';
 
 class Trash extends Component {
 
@@ -9,11 +9,23 @@ class Trash extends Component {
 
   constructor (props) {
     super(props);
+    this.showConfirmation = this.showConfirmation.bind(this);
+  }
+
+  showConfirmation () {
+    Alert.alert(
+      'Delete Note',
+      'Are you sure you want to delete this note?',
+      [
+        { text: 'Cancel', style: 'cancel' },
+        { text: 'Delete', onPress: () => this.props.remove(), style: 'destructive' },
+      ]
+    );
   }
   
   render () {
     return (
-      <TouchableOpacity onPress={ () => this.props.remove() }>
+      <TouchableOpacity onPress={ () => this.showConfirmation() }>
         <Image
           style={{ height: 20, width: 20 }}
           source={ require('../../../assets/icons/trash.png')}
