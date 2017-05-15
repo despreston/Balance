f031c3be9ef65afc23888b763751e5ae2594638f
despreston <despreston@gmail.com>
2017-05-15 15:31:55 -0400

    point to nginx server --skip-ci

--- client/BalanceApp/config/default.js
+++ client/BalanceApp/config/default.js
@@ -1,5 +1,5 @@
 export default {
-  apiUrl: "http://104.236.197.124:9000/",
+  apiUrl: "http://104.236.197.124:8080/",
   clientId: "i2jnTYoRsPs7br7KrMX52LgfH9YeThlz",
   domain: "balanceapp.auth0.com",
   piperSocket: "http://104.236.197.124:9001"
