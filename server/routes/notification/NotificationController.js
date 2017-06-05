4be6748a163f0191d0528574690a6be42ca755e4
despreston <despreston@gmail.com>
2017-06-04 22:25:50 -0400

    async/await conversion --skip-ci

--- server/routes/notification/NotificationController.js
+++ server/routes/notification/NotificationController.js
@@ -3,44 +3,50 @@ const log = require('logbro');
 
 module.exports = ({ get, post }) => {
 
-  get('notifications', ({ user }, res) => {
-    Notification
-    .find({ userId: user.sub })
-    .populate('related.item')
-    .then(notifications => res.send(200, notifications))
-    .catch(err => {
-      log.error(err);
+  get('notifications', async ({ user }, res) => {
+    try {
+      const notifications = await Notification
+        .find({ userId: user.sub })
+        .populate('related.item');
+
+      return res.send(200, notifications);
+    } catch (e) {
+      log.error(e);
       return res.send(500);
-    });
+    }
   });
 
-  post('notifications/read', ({ user }, res) => {
-    /**
-     * Returns an empty array because mongo `update` returns a WriteResult--
-     * not the updated documents.
-     */
-    Notification
-    .update(
-      { $and: [{ readAt: { $exists: false } }, { userId: user.sub }] },
-      { $set: { readAt: new Date() } },
-      { multi: true }
-    )
-    .then(() => res.send(200, []))
-    .catch(err => {
-      log.error(err);
+  post('notifications/read', async ({ user }, res) => {
+    try {
+      /**
+       * Returns an empty array because mongo `update` returns a WriteResult--
+       * not the updated documents.
+       */
+      await Notification
+        .update(
+          { $and: [{ readAt: { $exists: false } }, { userId: user.sub }] },
+          { $set: { readAt: new Date() } },
+          { multi: true }
+        );
+
+      return res.send(200, []);
+    } catch (e) {
+      log.error(e);
       return res.send(500);
-    });
+    }
   });
 
-  post('notifications/clear', ({ user }, res) => {
-    Notification
-    .find({ userId: user.sub })
-    .then(notifications => notifications.forEach(n => n.remove()))
-    .then(() => res.send(200, []))
-    .catch(err => {
-      log.error(err);
+  post('notifications/clear', async ({ user }, res) => {
+    try {
+      let notifications = await Notification.find({ userId: user.sub });
+
+      notifications.forEach(n => n.remove());
+
+      return res.send(200, []);
+    } catch (e) {
+      log.error(e);
       return res.send(500);
-    });
+    }
   });
 
 };
