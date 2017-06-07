7479407c65ede0f0b83b270ebc3843939f793370
despreston <despreston@gmail.com>
2017-06-07 12:08:07 -0400

    fixing address for piper-socket --skip-ci

--- client/BalanceApp/config/default.js
+++ client/BalanceApp/config/default.js
@@ -2,6 +2,6 @@ export default {
   apiUrl: "https://api.getbalanceapp.com/",
   clientId: "i2jnTYoRsPs7br7KrMX52LgfH9YeThlz",
   domain: "balanceapp.auth0.com",
-  piperSocket: "https://api.getbalanceapp.com/piper",
+  piperSocket: "https://api.getbalanceapp.com/",
   s3Bucket: "balanceapp"
 };
