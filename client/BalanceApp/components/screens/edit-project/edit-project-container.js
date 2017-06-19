44f7b246a8f30953a740412b9aa5beb875472873
despreston <despreston@gmail.com>
2017-06-19 11:44:22 -0400

    hopefully temporary fix for deleting notes. shitty solution --skip-ci

--- client/BalanceApp/components/screens/edit-project/edit-project-container.js
+++ client/BalanceApp/components/screens/edit-project/edit-project-container.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { NavigationActions } from 'react-navigation'
+import { NavigationActions } from 'react-navigation';
 import actions from '../../../actions/';
 import EditProject from './edit-project';
 import NavBtn from '../../navigation/nav-btn';
